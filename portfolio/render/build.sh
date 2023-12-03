1485398c6b2faf91d41efc38816d009cd66a6e2d
juanp <juanpcandal@gmail.com>
2023-12-03 20:57:35 -0300

    build7

--- portfolio/render/build.sh
+++ portfolio/render/build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
+npm cache clear --force
 npm i -g yarn
 npm i -g rimraf
 npm install cross-env
-npm install
 pip install -r ./portfolio/requirements.txt 
 python ./portfolio/manage.py collectstatic --no-input
 python ./portfolio/manage.py migrate
