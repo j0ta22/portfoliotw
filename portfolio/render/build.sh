eef5be176aaa8cfb03536406f8cd9ae7aa75e190
juanp <juanpcandal@gmail.com>
2023-12-03 20:47:06 -0300

    npm3

--- portfolio/render/build.sh
+++ portfolio/render/build.sh
@@ -5,6 +5,7 @@ set -o errexit
 npm i -g yarn
 npm i -g rimraf
 npm install cross-env
+npm install
 pip install -r ./portfolio/requirements.txt 
 python ./portfolio/manage.py collectstatic --no-input
 python ./portfolio/manage.py migrate
