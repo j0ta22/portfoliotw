fccd9f1d5c71d55c94679feff29642d1551d7412
juanp <juanpcandal@gmail.com>
2023-12-03 22:56:25 -0300

    settings2

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = "portfolio.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'portfolio/theme/templates')],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
