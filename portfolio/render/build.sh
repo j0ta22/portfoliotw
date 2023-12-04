1509773e62ce2fc245da1d488065927487dd703c
juanp <juanpcandal@gmail.com>
2023-12-03 23:09:58 -0300

    tailwind

--- portfolio/render/build.sh
+++ portfolio/render/build.sh
@@ -23,5 +23,6 @@ npm install --save-dev cross-env
 pip install django-environ
 pip install -r ./portfolio/requirements.txt 
 python ./portfolio/manage.py collectstatic --no-input
+python ./portfolio/manage.py tailwind init
 python ./portfolio/manage.py migrate
 python ./portfolio/manage.py tailwind build 
