1fcc8fa08b445fa3c0bb80e6f2c8b65d5c1a4172
juanp <juanpcandal@gmail.com>
2023-12-03 21:26:29 -0300

    buid8

--- portfolio/render/build.sh
+++ portfolio/render/build.sh
@@ -7,6 +7,7 @@ npm cache clear --force
 npm i -g yarn
 npm i -g rimraf
 npm install cross-env
+npm update
 pip install -r ./portfolio/requirements.txt 
 python ./portfolio/manage.py collectstatic --no-input
 python ./portfolio/manage.py migrate
