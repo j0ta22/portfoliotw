0219d40724a5d90268db61dd6223702ed5f5777d
juanp <juanpcandal@gmail.com>
2023-12-03 21:53:41 -0300

    build14

--- portfolio/render/build.sh
+++ portfolio/render/build.sh
@@ -13,6 +13,13 @@ npm install --save-dev tailwindcss
 npm install --save-dev postcss-simple-vars
 npm install --save-dev @tailwindcss/forms
 npm install --save-dev @tailwindcss/typography
+npm install --save-dev @tailwindcss/line-clamp
+npm install --save-dev @tailwindcss/aspect-ratio
+npm install --save-dev rimraf
+npm install --save-dev postcss-nested
+npm install --save-dev postcss-import
+npm install --save-dev postcss
+npm install --save-dev cross-env
 pip install -r ./portfolio/requirements.txt 
 python ./portfolio/manage.py collectstatic --no-input
 python ./portfolio/manage.py migrate
